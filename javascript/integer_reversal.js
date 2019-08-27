631e26d1eebd52e7e70939f1740dee9a35b0371c
eevan7a9 <e7a9@protonmail.com>
2019-08-27 22:26:58 +0900

    added solution for integer reverse javascript

--- javascript/integer_reversal.js
+++ javascript/integer_reversal.js
@@ -2,13 +2,34 @@
 // ex. 123 to 321
 
 // first solution
-function reverseInt(int) {
-  let reverse = String(int)
+function reverseInt_sol_1(int) {
+  return (
+    parseInt(
+      String(int)
+        .split("")
+        .reverse()
+        .join(""),
+    ) * Math.sign(int)
+  );
+}
+// second solution
+function reverseInt_sol_2(int) {
+  let reverse = "";
+  for (let i = 0; i < String(int).length; i++) {
+    const char = String(int)[i];
+    reverse = char + reverse;
+  }
+  return parseInt((reverse = int >= 0 ? reverse : "-" + reverse));
+}
+// third solution
+function reverseInt_sol_3(int) {
+  let reverse = "";
+  String(int)
     .split("")
-    .reverse()
-    .join("");
-  return reverse;
+    .forEach(char => (reverse = char + reverse));
+  return parseInt(reverse) * Math.sign(int);
 }
 
-const x = reverseInt(1234);
-console.log(x);
+const x = reverseInt_sol_2(-1234);
+const y = reverseInt_sol_2(2234);
+console.log(x, y);
